a89492c7337879bd1ebea15446f3da1e78afe312
JenMcD-star <jenniferannvancura@gmail.com>
2022-06-22 13:57:55 -0500

    Added gameOver function to stop the game if the score is 5 with an option to restart the game. Updated score tracker to add the round results together and display them.

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -2,6 +2,7 @@ const rock = document.getElementById("ROCK");
 const paper = document.getElementById("PAPER");
 const scissors = document.getElementById("SCISSORS");
 const computerChoice = ["ROCK", "PAPER", "SCISSORS"];
+const scores = document.getElementById('scores');
 
 
 function getComputer() {
@@ -27,24 +28,53 @@ scissors.addEventListener('click', function(){
   playRound("SCISSORS", computerSelection)
 })
 
-
-function playRound(playerSelection, computerSelection) {
 let playerScore = "0";
 let computerScore = "0";
+
+let isGameOver = (score) => {
+  if (playerScore === 5 || computerScore === 5) {
+    rock.setAttribute("disabled", 1);
+    paper.setAttribute("disabled", 1);
+    scissors.setAttribute("disabled", 1);
+    return true;
+    
+  }
+  else
+  return false;
+}
+function playAgain (){
+    playerScore = 0;
+    computerScore = 0;
+    rock.removeAttribute("disabled");  paper.removeAttribute("disabled");  scissors.removeAttribute("disabled");
+    return document.getElementById('end').innerHTML = ''
+  }
+
+ 
+function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    alert (`Tie! You both picked ${computerSelection}.`);
+    alerts.innerHTML = `Tie! You both picked ${computerSelection}.`;
   } else if (computerSelection === "ROCK" && playerSelection === "SCISSORS") {
   ++computerScore;
-    alert ("you lose! Rock beats scissors.");
+    alerts.innerHTML = "You lose! ROCK beats SCISSORS.";
   } else if (computerSelection === "PAPER" && playerSelection === "ROCK") {
     ++computerScore;
-    alert ("you lose! Paper beats rock.");
+    alerts.innerHTML = "You lose! PAPER beats ROCK.";
   } else if (computerSelection === "SCISSORS" && playerSelection === "PAPER") {
    ++computerScore;
-    alert("you lose! Scissors beats paper.");
+    alerts.innerHTML = "You lose! SCISSORS beats PAPER.";
   } else {
   ++playerScore;
-    alert (`You win! ${playerSelection} beats ${computerSelection}`);
+    alerts.innerHTML = `You win! ${playerSelection} beats ${computerSelection}`;
+  }
+  document.getElementById('scores').innerHTML =`Player Score: ${playerScore} Computer Score: ${computerScore}`
+  if ( isGameOver() ) {
+  if (computerScore > playerScore){
+    return document.getElementById('end').innerHTML = `Game over! You lose. <button onclick="playAgain()">Click here to play again</button>`;
+}else if (playerScore > computerScore){
+  return document.getElementById('end').innerHTML = `Game over! You win. <button onclick="playAgain()">Click here to play again</button>`;
+
+}
   }
 }
+
 
