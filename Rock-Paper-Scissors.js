ec5d7d557b5fffec49bf0216846c2a8bdff314cf
JenMcD-star <jenniferannvancura@gmail.com>
2022-06-22 11:34:36 -0500

    moved getComputer() under the eventlistener to get a new random value each time a button is clicked.

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,49 +1,50 @@
+const rock = document.getElementById("ROCK");
+const paper = document.getElementById("PAPER");
+const scissors = document.getElementById("SCISSORS");
 const computerChoice = ["ROCK", "PAPER", "SCISSORS"];
+
+
 function getComputer() {
   return computerChoice[Math.floor(Math.random() * 3)];
 }
 
-function getUser() {
-  let input = prompt("please enter rock, paper or scissors").toUpperCase();
-  return input;
-}
 
-function playRound() {
+rock.addEventListener('click', function() {
   let computerSelection = getComputer();
   console.log(computerSelection);
-  let playerSelection = getUser();
-  whoWon(playerSelection, computerSelection);
+  playRound("ROCK", computerSelection)
+});
 
-}
+paper.addEventListener('click', function(){
+let computerSelection = getComputer();
+  console.log(computerSelection);
+playRound("PAPER", computerSelection)
+})
+
+scissors.addEventListener('click', function(){
+  let computerSelection = getComputer();
+  console.log(computerSelection);
+  playRound("SCISSORS", computerSelection)
+})
+
+
+function playRound(playerSelection, computerSelection) {
 let playerScore = "0";
 let computerScore = "0";
-function whoWon(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     alert (`Tie! You both picked ${computerSelection}.`);
   } else if (computerSelection === "ROCK" && playerSelection === "SCISSORS") {
-    ++computerScore;
+  ++computerScore;
     alert ("you lose! Rock beats scissors.");
   } else if (computerSelection === "PAPER" && playerSelection === "ROCK") {
     ++computerScore;
     alert ("you lose! Paper beats rock.");
   } else if (computerSelection === "SCISSORS" && playerSelection === "PAPER") {
-    ++computerScore;
+   ++computerScore;
     alert("you lose! Scissors beats paper.");
   } else {
-    ++playerScore;
+  ++playerScore;
     alert (`You win! ${playerSelection} beats ${computerSelection}`);
   }
 }
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    playRound();
-    console.log(
-      `your score is ${playerScore}. The computer score is ${computerScore}`
-    );
-  }
-  if ((i = 5)) {
-    alert(`game over! Final score ${playerScore} to ${computerScore}`);
-  }
-}
-playGame();
